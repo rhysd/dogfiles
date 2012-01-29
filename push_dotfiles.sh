f4b8ace5b2a8600a019dce280e2073f574bd71a5
rhysd <u033263f@ecs.cmc.osaka-u.ac.jp>
2012-01-30 03:50:00 +0900

    updated at 2012年 1月30日 月曜日 03時50分00秒 JST

--- push_dotfiles.sh
+++ push_dotfiles.sh
@@ -11,8 +11,7 @@ do
         ;;
     esac
     cp -r $dotfile $dotfile_name
-    echo "cp -r ${dotfile} ${dotfile_name}"
 done
-git add .
+git add *
 git commit -m "updated at `date`"
 git push origin master
