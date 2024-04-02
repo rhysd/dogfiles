c2ac097bb65f7e8f50a95f93a79564686853ad62
rhysd <lin90162@yahoo.co.jp>
2024-04-02 17:02:22 +0900

    powershell: define `time` command

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,21 +1,27 @@
-if ($host.Name -eq 'ConsoleHost')
+if ($host.Name -eq "ConsoleHost")
 {
+    # Aliases
     Set-Alias g git
     Set-Alias v gvim
 
     # Set the default encoding of > and >>
-    $PSDefaultParameterValues['Out-File:Encoding'] = 'utf8'
+    $PSDefaultParameterValues["Out-File:Encoding"] = "utf8"
 
     # Environment variables
     $Env:HGREP_DEFAULT_OPTS = "--theme ayu-mirage --background"
 
     # Line editor config
     Import-Module PSReadLine
-
     Set-PSReadlineOption -EditMode Emacs
-
     Set-PSReadLineKeyHandler -Key Ctrl+i -Function Complete
     Set-PSReadLineKeyHandler -Key Ctrl+j -Function AcceptLine
     Set-PSReadLineKeyHandler -Key Ctrl+y -Function Paste
     Set-PSReadLineKeyHandler -Key Ctrl+d -Function DeleteChar
+
+    # Custom commands
+    function time() {
+        echo "Measuring `"${args}`"..."
+        $head, $tail = $args
+        Measure-Command { & $head $tail | Out-Default }
+    }
 }
