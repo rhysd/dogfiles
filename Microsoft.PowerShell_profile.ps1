b5e10fcd71c8808bd452fb416ddb44fde6dcaf9e
rhysd <lin90162@yahoo.co.jp>
2026-01-28 13:41:27 +0900

    powershell: tell Windows Terminal the current working directory
    when printing a prompt

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -29,4 +29,21 @@ if ($host.Name -eq "ConsoleHost")
         $head, $tail = $args
         Measure-Command { & $head $tail | Out-Default }
     }
+
+    # Send OSC 9;9 to tell the terminal the current working directory. This configuration is necessary to keep the
+    # current working directory when splitting a pane in Windows Terminal. Otherwise Windows Terminal opens a new pane
+    # with the starting directory instead of the current working directory.
+    # https://learn.microsoft.com/ja-jp/windows/terminal/tutorials/new-tab-same-directory
+    #
+    # Note: `$ExecutionContext.SessionState.Path.LocationChanged` is better but not available on PowerShell 5.1
+    function prompt {
+        $loc = $executionContext.SessionState.Path.CurrentLocation;
+
+        $out = ""
+        if ($loc.Provider.Name -eq "FileSystem") {
+            $out += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
+        }
+        $out += "PS $loc$('>' * ($nestedPromptLevel + 1)) ";
+        return $out
+    }
 }
